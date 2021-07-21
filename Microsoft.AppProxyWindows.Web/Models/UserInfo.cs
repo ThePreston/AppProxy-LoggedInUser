2c857032413ddf681ab8a8f67ae29dc9d42fd901
Preston Hale <prestonhale@microsoft.com>
2021-07-21 16:21:38 -0500

    Add project files.

---
+++ Microsoft.AppProxyWindows.Web/Models/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Microsoft.AppProxyWindows.Web.Models
+{
+    public class UserInfo
+    {
+        public List<Claim> claim { get; set; }
+
+        public string JSON { get; set; }
+
+    }
+}
